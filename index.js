cf1b377ff70a6c1b700bfd61e721990b85505c1a
NiravLakhani <Niravlakhani020@gmail.com>
2022-01-31 20:27:05 +0530

    Program Complited

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log("Hello");
+
+let a = [1 , 4 , 7 , 8 , 10];
+
+let b = [2 , 3 , 6];
+
+temp = [];
+
+for(let i = 0; i < a.length; i++){
+
+    if(a[i] > b[0]){
+        temp = b[0];
+        b[0] = a[i];
+        a[i] = temp;
+    }
+    b.sort(function(a, b){return a - b});
+    console.log("a" , a);
+    console.log("b" , b);
+    console.log("i" , i);
+}
